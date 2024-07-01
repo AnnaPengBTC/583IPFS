31fd7836e51921e0dce1b8975e54cae207e9dad8
AnnaPengBTC <annapeng@seas.upenn.edu>
2024-07-01 08:06:19 -1000

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -10,8 +10,8 @@ def pin_to_ipfs(data):
   }
 	url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
 	headers = {
-		'pinata_apikey: 37e6ad229f4345d03996',
-		'pinata_secret_apikey: a1d3e4dea02a3d298cc71a05669b098c747f801276df39ba9d355f8464c9cdb5'
+		'pinata_apikey': '37e6ad229f4345d03996',
+		'pinata_secret_apikey': 'a1d3e4dea02a3d298cc71a05669b098c747f801276df39ba9d355f8464c9cdb5'
   }
 	response = requests.post(url, files = files, headers=headers)
 	cid = response.json()["IpfsHash"]
@@ -22,8 +22,8 @@ def get_from_ipfs(cid,content_type="json"):
 	#YOUR CODE HERE	
 	url = f"https://gateway.pinata.cloud/ipfs/{cid}"
 	headers = {
-		'pinata_apikey: 37e6ad229f4345d03996',
-		'pinata_secret_apikey: a1d3e4dea02a3d298cc71a05669b098c747f801276df39ba9d355f8464c9cdb5'
+		'pinata_apikey': '37e6ad229f4345d03996',
+		'pinata_secret_apikey': 'a1d3e4dea02a3d298cc71a05669b098c747f801276df39ba9d355f8464c9cdb5'
     }
 	response = requests.get(url, headers=headers)
 	if content_type == "json":
